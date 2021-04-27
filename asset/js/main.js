fabb358f16a2a02b41cfd744e946fb84f1564b2c
valeriomatranga <matrangav@gmail.com>
2021-04-27 15:49:26 +0200

    ciclo numeri

---
+++ asset/js/main.js
@@ -0,0 +1,16 @@
+//numero da 1 a 100
+var numero;
+
+for (var numero = 1; numero < 101; numero++){
+    console.log(numero);
+}
+
+//fizz
+var multiplo3 = fizz;
+
+//buzz
+var multiplo5 = buzz;
+
+//fizzBuzz
+var multiplo3_5 = fizzBuzz;
+
